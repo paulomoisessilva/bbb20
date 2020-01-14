4f059b6a26c946f9e3b6d21a2e0fdb75a8f6df26
paulomoisessilva <33613206+paulomoisessilva@users.noreply.github.com>
2020-01-14 02:32:09 -0200

    trying add things

--- inicio.js
+++ inicio.js
@@ -1,3 +1,25 @@
 $(function(){
     $("#titulo").append(" - Estatísticas")
 })
+
+
+
+$(function(){
+    $("#concorrentes")
+})
+
+
+$(function(){
+    setInterval(function(){
+        var tamanho1 = $(".imagem-participante").width()
+        var div = $(".imagem-participante")
+        var imagem = $($(div).children("img"))
+        var tamanho2 = $(imagem).width()
+        if(tamanho1>=tamanho2){
+            $(imagem).removeClass("img2").addClass("img1")
+        }else{
+            $(imagem).addClass("img2").removeClass("img1")
+        }
+    },100)
+})
+
