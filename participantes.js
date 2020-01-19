dd60b123affe135c4ab15717f2d94b298a89b45c
paulomoisessilva <33613206+paulomoisessilva@users.noreply.github.com>
2020-01-19 17:44:15 -0200

    Update participantes.js

--- participantes.js
+++ participantes.js
@@ -4,6 +4,23 @@ $(function() {
     $("#participante").addClass("active")
 })
 
+$(function() {
+    $.getJSON("https://script.google.com/macros/s/AKfycbxEivVIGQ84tnZTYOcB6TeP4kdp522bzbQW9G1fL7WlfzrYKDM/exec?func=get", function(data) {
+        var nome = Object.keys(data)[0]
+        var dados = data[nome]
+
+        var ultimo = dados[dados.length - 1]
+        dadosGerais = dados
+
+        var lastDate = new Date(ultimo[0])
+        var lastDateText = "<strong>Última Atualização:</strong> " + lastDate.toLocaleString()
+
+        $("#horario").html(lastDateText)
+
+        addParedoes(ultimo)
+
+    })
+})
 
 $(function() {
     var url = "https://script.google.com/macros/s/AKfycbxEivVIGQ84tnZTYOcB6TeP4kdp522bzbQW9G1fL7WlfzrYKDM/exec?func=getParticipantes"
